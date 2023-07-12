4917f66f7fa1f74432eae1bc217f0bb88da94940
Arch-Noize <santiagonoize@gmail.com>
2023-07-12 14:25:10 -0400

    Test editing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import './index.css';
 /* Selectors */
 
 const items = JSON.parse(localStorage.getItem('items')) || [];
-const addBtn = document.querySelector("#add-btn");
 const list = document.querySelector('#list');
+const addBtn = document.querySelector("#add-btn");
+const removeBtn = document.querySelectorAll("#remove");
 
 /* To-do list displaying and storing */
 
@@ -12,7 +13,13 @@ const todoList = () => {
     items.sort((a, b) => a.index - b.index);
   
     items.forEach((item) => {
-        list.innerHTML += `<tr><td><input type="checkbox"><span class="item">${items.desc}</span></td></tr>`;
+        list.innerHTML += `
+        <tr>
+            <td>
+            <input type="checkbox"><span class="item">${item.desc}</span> 
+            <button id="remove"> <i class="fa fa-times"></i> </button>
+            </td>
+        </tr>`;
     });
 };
 
@@ -22,7 +29,7 @@ const storeItem = () => {
   
 todoList();
 
-/* To-do list functions */
+/* Add Item */
 
 const addItem = (desc) => {
     const item = {
@@ -34,24 +41,45 @@ const addItem = (desc) => {
     storeItem();
 } 
 
-const removeTask = (index) => {
-    items.splice(index, 1);
-    for (let i = index; i < tasks.length; i += 1) {
-      items[i].index = i + 1;
-    }
-    storeTasks();
-  }
-
-/* Buttons */
-
 addBtn.addEventListener('click', () => {
     const newItem = document.querySelector("#new").value;
     addItem(newItem);
     list.innerHTML += `
     <tr>
         <td> 
-            <input type="checkbox"> <span class="item">${newItem}</span>
+            <input type="checkbox"> <span class="item">${newItem}</span>  
+            <button id="remove"> <i class="fa fa-times"></i> </button> 
         </td>
     </tr>`;
     document.querySelector("#new").value = '';
 })
+
+/* Remove Item */
+
+// const removeItem = (index) => {
+//     items.splice(index, 1);
+//     for (let i = index; i < items.length; i += 1) {
+//       items[i].index = i + 1;
+//     }
+//     storeItem();
+// }
+
+// removeBtn.addEventListener('click', () => {
+
+// })
+
+/* Edit Item */
+
+const editItem = (index, desc) => {
+    items[index].desc = desc;
+    storeItem();
+}
+
+document.querySelectorAll('.item').forEach((item) => {
+    item.addEventListener('click', (event) => {
+        console.log('test');
+      const desc = event.target.textContent;
+      const index = [...item.parentElement.children].indexOf(item) - 1;
+      editItem(index, desc);
+    });
+});
