7ea7d1cdd2c3996fb5d30a68cb9cbb572f6fd9a7
Arch-Noize <santiagonoize@gmail.com>
2023-07-12 05:54:46 -0400

    Add linter files and fix errors

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import './index.css';
 
 const items = [
-    {
-      desc: 'Clean around.',
-      completed: false,
-      index: 1,
-    },
-    {
-      desc: 'Clean yourself!',
-      completed: false,
-      index: 2,
-    },
-    {
-      desc: 'Cook around.',
-      completed: false,
-      index: 3,
-    },
-    {
-      desc: 'Cook yours-! Wait no. Dont do that',
-      completed: true,
-      index: 4,
-    },
-  ];
-  
+  {
+    desc: 'Clean around.',
+    completed: false,
+    index: 1,
+  },
+  {
+    desc: 'Clean yourself!',
+    completed: false,
+    index: 2,
+  },
+  {
+    desc: 'Cook around.',
+    completed: false,
+    index: 3,
+  },
+  {
+    desc: 'Cook yours-! Wait no. Dont do that',
+    completed: true,
+    index: 4,
+  },
+];
+
 const todoList = () => {
-    const list = document.querySelector('#list');
-  
-    items.sort((a, b) => a.index - b.index);
-  
-    items.forEach((item) => {
-        list.innerHTML += `<tr><td><input type="checkbox"><span class="item">${item.desc}</span></td></tr>`;
-    })
-}
+  const list = document.querySelector('#list');
+
+  items.sort((a, b) => a.index - b.index);
+
+  items.forEach((item) => {
+    list.innerHTML += `<tr><td><input type="checkbox"><span class="item">${item.desc}</span></td></tr>`;
+  });
+};
 
 todoList();
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: path.resolve(__dirname, 'src', 'index.js'),
-    },
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-            {
-            test: /.css$/i,
-            use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
+  },
 };
