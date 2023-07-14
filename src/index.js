b7d408f419f0eea4d030924d485aa6e6b4e899ce
Arch-Noize <santiagonoize@gmail.com>
2023-07-14 11:40:23 -0400

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,107 +1,105 @@
+/* eslint-disable no-alert, no-plusplus, */
+
 import './index.css';
 
 /* Selectors */
 
 const todo = JSON.parse(localStorage.getItem('items')) || [];
 const list = document.querySelector('#list');
-const addBtn = document.querySelector("#add-btn");
+const addBtn = document.querySelector('#add-btn');
 
 const storeItem = () => {
-    localStorage.setItem('items', JSON.stringify(todo));
-} 
+  localStorage.setItem('items', JSON.stringify(todo));
+};
 
 /* To-do list displaying and storing */
 
 const todoList = () => {
-    todo.sort((a, b) => a.index - b.index);
-  
-    todo.forEach((item) => {
-        list.innerHTML += `
+  todo.sort((a, b) => a.index - b.index);
+
+  todo.forEach((item) => {
+    list.innerHTML += `
         <li> 
             <input type="checkbox"><span class="item">${item.desc}<i class="fa fa-ellipsis-v"></i></span> 
         </li>`;
-    });
-
+  });
 };
 
 /* Add Item */
 
 const addItem = (desc) => {
-    const item = {
-        desc,
-        completed: false,
-        index: todo.length + 1,
-    }
-    todo.push(item);
-    storeItem();
-} 
+  const item = {
+    desc,
+    completed: false,
+    index: todo.length + 1,
+  };
+  todo.push(item);
+  storeItem();
+};
 
 addBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const newItem = document.querySelector("#new").value;
-    if(!newItem){
-        alert("Please add a task!")
-    } else {
-        addItem(newItem);
-        list.innerHTML += `
+  e.preventDefault();
+  const newItem = document.querySelector('#new').value;
+  if (!newItem) {
+    alert('Please add a task!');
+  } else {
+    addItem(newItem);
+    list.innerHTML += `
             <li> 
             <input type="checkbox"><span class="item">${newItem}<i id="edit" class="fa fa-ellipsis-v"></i></span> 
             </li>`;
-        document.querySelector("#new").value = '';
-    }
-    storeItem();
-})
+    document.querySelector('#new').value = '';
+  }
+  storeItem();
+});
 
 /* Remove Item */
 
 const removeItem = (index) => {
-    todo.splice(index, 1);
-    for (let i = index; i < todo.length; i++) {
-        todo[i].index = i + 1;
-      }
-    storeItem();
-}
+  todo.splice(index, 1);
+  for (let i = index; i < todo.length; i++) {
+    todo[i].index = i + 1;
+  }
+  storeItem();
+};
 
 /* Edit Item */
 
 const editItem = (index, desc) => {
-    todo[index].desc = desc;
-    console.log("I was done!")
-    storeItem();
-}
+  todo[index].desc = desc;
+  storeItem();
+};
 
 /* Find Index */
 
 const findIndex = (e) => {
-    let items = document.querySelectorAll('.item');
-    let index = 0;
-    for (let i = 0; i < items.length; i++){
-        let text = e.target.parentElement.textContent;
-        if (text === todo[i].desc){
-            index = i;
-        }
+  const items = document.querySelectorAll('.item');
+  let index = 0;
+  for (let i = 0; i < items.length; i++) {
+    const text = e.target.parentElement.textContent;
+    if (text === todo[i].desc) {
+      index = i;
     }
-    return index;
-}
+  }
+  return index;
+};
 
 /* List Listeners */
 
 list.addEventListener('click', (e) => {
-    let index = findIndex(e);
-    if (e.target.classList.contains("fa-ellipsis-v")){
-        e.target.parentElement.contentEditable = 'true';
-        e.target.parentElement.addEventListener('input', () => {
-            console.log("i was pressed");
-            todo[index].desc = e.target.parentElement.textContent;
-            console.log(e.target.parentElement.textContent);
-            storeItem();
-        })
-        e.target.classList.remove("fa-ellipsis-v");
-        e.target.classList.add("fa-times");
-    } else if (e.target.classList.contains("fa-times")){
-        removeItem(index);
-        e.target.parentElement.parentElement.remove();
-    }
-})
+  const index = findIndex(e);
+  if (e.target.classList.contains('fa-ellipsis-v')) {
+    e.target.parentElement.contentEditable = 'true';
+    e.target.parentElement.addEventListener('input', () => {
+      editItem(index, e.target.parentElement.textContent);
+      storeItem();
+    });
+    e.target.classList.remove('fa-ellipsis-v');
+    e.target.classList.add('fa-times');
+  } else if (e.target.classList.contains('fa-times')) {
+    removeItem(index);
+    e.target.parentElement.parentElement.remove();
+  }
+});
 
-window.addEventListener("DOMContentLoaded", todoList());
+window.addEventListener('DOMContentLoaded', todoList());
